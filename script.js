144194375690a082910d330766a7f2615378374f
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-06-24 12:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-//your JS code here. If required.
+// Fetch a random quote from the API
+function fetchRandomQuote() {
+	fetch('http://api.quotable.io/random')
+		.then(response => response.json())
+		.then(data => {
+			document.getElementById('quote').textContent = data.content;
+			startTimer();
+		})
+		.catch(error => console.log(error));
+}
+
+// Check if the typed text matches the quote
+function checkTypedText() {
+	const quoteInput = document.getElementById('quoteInput');
+	const quoteText = document.getElementById('quote').textContent;
+	const typedText = quoteInput.value;
+
+	if (typedText === quoteText) {
+		quoteInput.style.backgroundColor = 'green';
+		clearInputArea();
+		fetchRandomQuote();
+		setTimeout(() => {
+			quoteInput.style.backgroundColor = '';
+		}, 3000);
+	} else {
+		quoteInput.style.backgroundColor = 'red';
+	}
+}
+
+// Clear the input area and reset the timer
+function clearInputArea() {
+	document.getElementById('quoteInput').value = '';
+	document.getElementById('timer').textContent = '0';
+}
+
+// Start the timer
+function startTimer() {
+	let seconds = 0;
+	const timer = document.getElementById('timer');
+	timer.textContent = seconds;
+
+	setInterval(() => {
+		seconds++;
+		timer.textContent = seconds;
+	}, 1000);
+}
+
+// Event listener for typing input
+document.getElementById('quoteInput').addEventListener('input', checkTypedText);
+
+// Fetch the initial random quote
+fetchRandomQuote();
